922a2359b62ac68aa8a707aa730fff65d5b7b4d7
Sahil Banswani <sahilbanswani03@gmail.com>
2023-05-14 00:42:01 +0530

    second commit

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nike_shop_app/modules/shoes.dart';
-import 'package:nike_shop_app/pages/shop_page.dart';
 import 'package:provider/provider.dart';
 import '../components/cart_items.dart';
 import '../modules/cart.dart';
@@ -11,29 +10,31 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<Cart>(
-        builder: (context, value, child) => Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(children: [
-                //heading
-                const Text(
-                  'My Cart',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            //heading
+            const Text(
+              'My Cart',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
 
-                const SizedBox(
-                  height: 10,
-                ),
+            const SizedBox(
+              height: 10,
+            ),
 
-                Expanded(
-                    child: ListView.builder(
-                      itemCount: value.getUserCart().length,
-                      
-                    itemBuilder: (context, index) {
-                      Shoes shoes = value.getUserCart()[index];
-                      return CartItem(shoe: shoes);
-                }
-            )
-          ),
-  ])));
+            Expanded(
+              child: ListView.builder(
+                  itemCount: value.getUserCart().length,
+                  itemBuilder: (context, index) {
+                    Shoes shoes = value.getUserCart()[index];
+                    return CartItem(shoe: shoes);
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
